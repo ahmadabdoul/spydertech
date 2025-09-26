66056d506ffd00ce63814f3dfc5723c4d109d235
ahmadabdoul <44808233+ahmadabdoul@users.noreply.github.com>
2025-09-26 23:59:09 +0100

    Update connection.php

--- backend/assets/connection.php
+++ backend/assets/connection.php
@@ -2,7 +2,7 @@
 
 $servername = 'localhost';
 $username = "spidert5_root";
-$password = "xj4HWzgVrX_DK@f";
+$password = "";
 $dbname = "spidert5__spydertech";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Error connecting to mysql server");
